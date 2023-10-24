ab4f8ff0f816d022426393eeedf73899be969e1d
Faralessa <139886349+Faralessa@users.noreply.github.com>
2023-10-25 00:30:37 +0300

    Update Group_Project.py

--- Group_Project.py
+++ Group_Project.py
@@ -1,33 +1,40 @@
 # pip install streamlit pandas matplotlib seaborn plotly sqlalchemy
+# pip install openai
 
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
-from sqlalchemy import create_engine
-import openai
 import sqlite3
 import csv
+import openai
+import os
 
-# Convert CSV to SQLite
-def csv_to_sqlite(csv_file, db_name, table_name):
+# Read CSV directly
+df = pd.read_csv("Bank_Customer_retirement.csv")
+
+# Title
+st.title('Bank Customer Retirement Data Analysis')
+
+# Introduction
+intro = """
+As a collaborative data science team at a leading New York City bank, we aim to build a predictive model to determine a customer's retirement readiness. Our analysis involves exploring essential customer data, including age and 401K savings, to better understand the key factors influencing retirement preparedness. Through statistical analysis, correlation assessments, SQL querying, and GPT-4 integration, we seek to derive actionable insights that will inform the development of an accurate predictive model.
+"""
+st.write(intro)
+
+# Show original dataset
+st.subheader('Original Dataset')
+st.write(df)
+
+# Convert DataFrame to SQLite
+def df_to_sqlite(df, db_name, table_name):
     conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-
-    # Read CSV and insert into SQLite table
-    with open(csv_file, 'r') as file:
-        csv_reader = csv.reader(file)
-        header = next(csv_reader)
-        header = [f'"{col.replace(" ", "_").replace("/", "_")}"' if col[0].isdigit() else col.replace(" ", "_").replace("/", "_") for col in header]  # Modify column names
-        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join([f'{col} TEXT' for col in header])})")
-        cursor.executemany(f"INSERT INTO {table_name} VALUES ({', '.join(['?'] * len(header))})", csv_reader)
-
-    conn.commit()
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
     conn.close()
 
-# Convert CSV to SQLite
-csv_to_sqlite("Bank_Customer_retirement.csv", "customer_data.sqlite", "customer_table")
+# Convert DataFrame to SQLite
+df_to_sqlite(df, "customer_data.sqlite", "customer_table")
 
 # Load data from SQLite
 conn = sqlite3.connect('customer_data.sqlite')
@@ -36,32 +43,16 @@ cursor.execute("SELECT * FROM customer_table")
 df = pd.DataFrame(cursor.fetchall(), columns=[i[0] for i in cursor.description])
 conn.close()
 
-# Title
-st.title('Bank Customer Retirement Data Analysis')
-
-# Introduction
-intro = """
-As a collaborative data science team at a leading New York City bank, we aim to build a predictive model to determine a customer's retirement readiness. Our analysis involves exploring essential customer data, including age and 401K savings, to better understand the key factors influencing retirement preparedness. Through statistical analysis, correlation assessments, SQL querying, and GPT-3.5 integration, we seek to derive actionable insights that will inform the development of an accurate predictive model.
-"""
-st.write(intro)
-
-# Show dataset
-st.subheader('Dataset')
-st.write(df)
-
-# Drop the 'Customer ID' column
-df.drop('Customer_ID', axis=1, inplace=True)
-
-# Round the numeric columns to 2 digits after the decimal point
-df = df.round(2)
-
 # Add tabs
-tabs = ["Data Analysis", "Correlation Analysis", "SQL Query", "GPT-3.5 Integration"]
+tabs = ["Data Analysis", "Correlation Analysis", "SQL Query", "GPT-4 Integration"]
 choice = st.sidebar.radio("Select a tab", tabs)
 
 # Data Analysis tab
 if choice == "Data Analysis":
     st.subheader('Data Analysis')
+
+    # Drop the 'Customer ID' column
+    df.drop('Customer ID', axis=1, inplace=True)
 
     # Mention the reason for dropping the 'Customer ID' column
     st.write("We have dropped the 'Customer ID' column before the analysis to eliminate any potential bias or misinterpretation that could arise from the analysis of an identifier column.")
@@ -89,17 +80,18 @@ if choice == "Data Analysis":
     fig = px.histogram(df, x='Age', nbins=15)
     st.plotly_chart(fig)
 
-        # Explanation of Age Distribution
-    st.write("The concentration of approximately 113 customers aged between 42.5 and 47.49, as depicted by the Plotly histogram, holds significance for our data science team. Understanding the distribution of customers within this key age bracket is crucial in developing an accurate predictive model for assessing retirement readiness. By focusing on this prominent customer segment, we can tailor our predictive model to provide targeted insights and personalized financial strategies, ensuring effective retirement planning for individuals within this core age group.")
-
+    # Explanation of Age Distribution
+    st.write("The concentration of approximately 114 customers aged between 42.5 and 47.49, as depicted by the Plotly histogram, holds significance for our data science team. Understanding the distribution of customers within this key age bracket is crucial in developing an accurate predictive model for assessing retirement readiness. By focusing on this prominent customer segment, we can tailor our predictive model to provide targeted insights and personalized financial strategies, ensuring effective retirement planning for individuals within this core age group.")
+    
 # Correlation Analysis tab
 elif choice == "Correlation Analysis":
     st.subheader('Correlation Analysis')
-    corr = df.corr().round(2)
-    st.write(corr)
+
+    # Drop the 'Customer ID' column
+    df.drop('Customer ID', axis=1, inplace=True)
 
     # Display correlation as an interactive heatmap
-    st.subheader('Correlation Heatmap')
+    corr = df.corr().round(2)
     import plotly.figure_factory as ff
     fig = ff.create_annotated_heatmap(
         z=corr.values,
@@ -159,58 +151,73 @@ elif choice == "SQL Query":
 
     # Save DataFrame to SQLite database
     conn = sqlite3.connect('customer_data.sqlite')
-    df.to_sql('customer_table', conn, if_exists='replace', index=False)
+    try:
+        df.to_sql('customer_table', conn, if_exists='replace', index=False)
 
-    # Example query
-    example_query = '''SELECT Age, "401K Savings"
-    FROM customer_table
-    WHERE "401K Savings" > 800000
-    ORDER BY Age
-    LIMIT 5;'''
-    st.markdown(f"_Example:_ {example_query}")
+        # Example query
+        example_query = '''SELECT Age, "401K Savings"
+        FROM customer_table
+        WHERE "401K Savings" > 800000
+        ORDER BY Age
+        LIMIT 5;'''
+        st.markdown(f"_Example:_ {example_query}")
 
-    # Input for SQL query
-    user_query = st.text_area("Enter your SQL query:", value="", height=100)
+        # Input for SQL query
+        user_query = st.text_area("Enter your SQL query:", value="", height=100)
 
-    # Execute the query and display results
-    if st.button('Run Query'):
-        try:
-            result = conn.execute(user_query).fetchall()
-            column_names = [description[0] for description in conn.execute(user_query).description]
-            st.write("Results of the SQL Query:")
-            st.table(pd.DataFrame(result, columns=column_names))
-        except Exception as e:
-            st.write(f"An error occurred: {e}")
+        # Execute the query and display results
+        if st.button('Run Query'):
+            try:
+                result = conn.execute(user_query).fetchall()
+                column_names = [description[0] for description in conn.execute(user_query).description]
+                st.write("Results of the SQL Query:")
+                st.table(pd.DataFrame(result, columns=column_names))
+            except Exception as e:
+                st.write(f"An error occurred: {e}")
+    except Exception as e:
+        st.write(f"An error occurred while trying to save the DataFrame to the SQLite database: {e}")
+    finally:
+        # Close the connection
+        conn.close()
 
-    # Close the connection
-    conn.close()
+# GPT-4 Integration tab
+elif choice == "GPT-4 Integration":
+    st.subheader('GPT-4 Integration')
 
-# GPT-3.5 Integration tab
-elif choice == "GPT-3.5 Integration":
-    st.subheader('GPT-3.5 Integration')
+    try:
+        # Load OpenAI key from file
+        with open('key.txt', 'r') as file:
+            openai_key = file.read().strip()
 
-    # Load OpenAI key from file
-    with open('key.txt', 'r') as file:
-        openai_key = file.read().strip()
+        # Set OpenAI API key for GPT-4
+        openai.api_key = openai_key
 
-    # Set OpenAI API key for GPT-3.5
-    openai.api_key = openai_key
+        # Introduction to GPT-4 integration
+        st.write("Considering the crucial interplay of age, financial readiness, and retirement planning, we invite you to delve deeper into the dynamics of securing a stable financial future. With our ongoing analysis highlighting key trends in retirement preparation, we are eager to address any inquiries or uncertainties you may have about optimizing your financial plans for a comfortable retirement. Feel free to ask any questions or seek personalized insights to ensure a seamless transition into this significant phase of your life.")
 
-    # Introduction to GPT-3.5 integration
-    st.write("Considering the crucial interplay of age, financial readiness, and retirement planning, we invite you to delve deeper into the dynamics of securing a stable financial future. With our ongoing analysis highlighting key trends in retirement preparation, we are eager to address any inquiries or uncertainties you may have about optimizing your financial plans for a comfortable retirement. Feel free to ask any questions or seek personalized insights to ensure a seamless transition into this significant phase of your life.")
+        # Example related question in smaller font
+        st.markdown("*Example:* What are some effective strategies for maximizing my 401K savings as I approach retirement age?", unsafe_allow_html=True)
 
-    # Example related question in smaller font
-    st.markdown("*Example:* What are some effective strategies for maximizing my 401K savings as I approach retirement age?", unsafe_allow_html=True)
+        # Get user input
+        user_input = st.text_input("Enter your question:", value='')
 
-    # Get user input
-    user_input = st.text_input("Enter your question:", value='')
+        # Defining the message based on the user input
+        message = [{"role": "user", "content": user_input}]
 
-    # Call GPT-3.5 API
-    if user_input:
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=user_input,
-            max_tokens=150
-        )
-        st.write("*GPT-3.5's Response:*")
-        st.write(response.choices[0].text)
+        # Call GPT-4 API
+        if user_input:
+            print("User Input:", user_input)  # Add this line to print the user input
+            response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=message,
+                frequency_penalty=1.0,
+                max_tokens=150,
+                temperature=0.5
+            )
+            print("GPT-4's Response:", response.choices[0].message.content)  # Add this line to print the GPT-4 response
+            st.write("*GPT-4's Response:*")
+            st.write(response.choices[0].message.content)
+    except FileNotFoundError as e:
+        st.write("Error: Key file not found. Please make sure the key file is in the correct directory.")
+    except Exception as e:
+        st.write(f"An error occurred: {e}")
